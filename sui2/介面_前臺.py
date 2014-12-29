7f5ec11cfe4fcbf18e194b6a675bcdbb3debaf87
薛丞宏 <ihcaoe@gmail.com>
2014-12-29 12:40:49 +0800

    可以顯示首頁與登入登出按鈕了

--- sui2/介面_前臺.py
+++ sui2/介面_前臺.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.template import loader
+from django.template.context import RequestContext
+from django.http.response import HttpResponse
 
-def 確認登入資料(request):
-    pass
+def 首頁(request):
+# 	output = ', '.join([p.title for p in latest_poll_list])
+# 	return HttpResponse(output)
+	template = loader.get_template('index.html')
+	context = RequestContext(request, {	})
+	return HttpResponse(template.render(context))
